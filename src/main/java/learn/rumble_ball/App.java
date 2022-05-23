3ed34d0b6036471001511e3a99a85e8130b00907
EgeGurcanKula <egegkula@gmail.com>
2022-05-23 14:24:46 -0500

    App java

---
+++ src/main/java/learn/rumble_ball/App.java
@@ -0,0 +1,12 @@
+package learn.rumble_ball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
