c2d835b5280590b75cd0480412f76bb8e7d0b2a5
EgeGurcanKula <egegkula@gmail.com>
2022-05-24 15:34:28 -0500

    some changes

--- api/src/main/java/learn/rumble_ball/data/PostJdbcTemplateRepository.java
+++ api/src/main/java/learn/rumble_ball/data/PostJdbcTemplateRepository.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class PostJdbcTemplateRepository implements PostRepository {
@@ -23,15 +24,28 @@ public class PostJdbcTemplateRepository implements PostRepository {
 
     @Override
     public List<Post> findAll(){
-        final String sql = "select post_id, title, middle_name, last_name, dob, height_in_inches "
+        final String sql = "select post_id, type_id, title, post_date "
                 + "from post limit 1000;";
         return jdbcTemplate.query(sql, rowMapper);
 
     }
 
     @Override
+    @Transactional
     public Post findById(int id){
 
+        final String sql = "select post_id, type_id, title, post_date "
+                + "from post "
+                + "where post_id = ?;";
+
+        Post post = jdbcTemplate.query(sql, new PostMapper(), id).stream()
+                .findFirst().orElse(null);
+
+        if (post != null) {
+            addAgencies(post);
+        }
+
+        return post;
     }
 
 }
