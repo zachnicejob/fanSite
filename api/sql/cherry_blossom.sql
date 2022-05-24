e08f88190029b7f01acf2da3d1cd695e253bf567
Zach Nicely <zachnicel16@gmail.com>
2022-05-24 16:19:42 -0400

    inital test added

--- api/sql/cherry_blossom.sql
+++ api/sql/cherry_blossom.sql
@@ -8,24 +8,24 @@ create table post (
 	title varchar(50) not null,
     date_posted date not null,
     text_body varchar(10000),
-    image varchar(200)
+    image_link varchar(200)
 );
 
 
 -- auth
 
 
-insert into app_role (`name`) values
-    ('USER'),
-    ('ADMIN');
+-- insert into app_role (`name`) values
+--     ('USER'),
+--     ('ADMIN');
 
--- passwords are set to "P@ssw0rd!"
-insert into app_user (username, password_hash, disabled)
-    values
-    ('john@smith.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 0),
-    ('sally@jones.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 0);
+-- -- passwords are set to "P@ssw0rd!"
+-- insert into app_user (username, password_hash, disabled)
+--     values
+--     ('john@smith.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 0),
+--     ('sally@jones.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 0);
 
-insert into app_user_role
-    values
-    (1, 2),
-    (2, 1);
+-- insert into app_user_role
+--     values
+--     (1, 2),
+--     (2, 1);
---
+++ api/src/main/java/learn/rumble_ball/data/PostJdbcTemplateRepository.java
@@ -0,0 +1,35 @@
+package learn.rumble_ball.data;
+
+import learn.rumble_ball.data.mappers.PostMapper;
+import learn.rumble_ball.models.Post;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PostJdbcTemplateRepository implements PostRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private final PostMapper rowMapper = new PostMapper();
+
+    public PostJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Post> findAll(){
+        final String sql = "select post_id, title, middle_name, last_name, dob, height_in_inches "
+                + "from post limit 1000;";
+        return jdbcTemplate.query(sql, rowMapper);
+
+    }
+
+    @Override
+    public Post findById(int id){
+
+    }
+
+}
