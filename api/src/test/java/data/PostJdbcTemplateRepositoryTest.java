a2ccbdb1615d26fa492941f72fde0f138087dfd5
Zach Nicely <zachnicel16@gmail.com>
2022-05-25 16:12:48 -0400

    Security dependencies eliminated, findAll test built

--- api/src/test/java/data/PostJdbcTemplateRepositoryTest.java
+++ api/src/test/java/data/PostJdbcTemplateRepositoryTest.java
@@ -1,7 +1,14 @@
 package data;
 
+import learn.rumble_ball.data.PostRepository;
+import learn.rumble_ball.models.Post;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,5 +16,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class PostJdbcTemplateRepositoryTest {
 
     @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    PostRepository repository;
+
+    static boolean hasRun = false;
+
+    @BeforeEach
+    void setup() {
+        if (!hasRun) {
+            jdbcTemplate.update("call set_known_good_state();");
+            hasRun = true;
+        }
+    }
+
+    @Test
+    void shouldFindAllPosts() {
+        List<Post> allPosts = repository.findAll();
+
+        assertNotNull(allPosts);
+    }
 
 }
