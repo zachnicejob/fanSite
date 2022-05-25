a5ba6a5e3163ccb00b6723f483a78672cfea8508
Jared Guzman <jarededwardguzman25@gmail.com>
2022-05-25 12:37:16 -0700

    Homepage update and about page completion

--- ui/src/components/Homepage.js
+++ ui/src/components/Homepage.js
@@ -1,9 +1,19 @@
 
 function Homepage() {
  return (
-    <p className="text-3xl font-bold">
-      Hello, Homepage!
-    </p>
+  <>
+  <div className="hero min-h-[50%] py-16">
+    <div className="hero-content text-center">
+      <div className="max-w-md">
+        <h1 className="text-5xl font-bold text-metal">Welcome</h1>
+      </div>
+    </div>
+  </div>
+  <div className="max-w-md mx-auto">
+        <p className="py-4 text-metal">To Project Cherry Blossom,</p>
+        <p className="py-4 text-metal">A fansite dedicated to the One Piece character and doctor of the Straw Hat Pirate Crew: Tony Tony Chopper.</p>
+  </div>
+  </>
  )
 }
 
