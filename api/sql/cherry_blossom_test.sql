eddf5687098e05cfc9cc6b2729487d0f06da092a
EgeGurcanKula <egegkula@gmail.com>
2022-05-26 16:01:29 -0500

    test addition

--- api/sql/cherry_blossom_test.sql
+++ api/sql/cherry_blossom_test.sql
@@ -40,7 +40,8 @@ insert into post (type_id, title, post_date, text_body, image_link)
     (1, 'First Event!', '2022-04-25', 'This is the first event post', 'http://pm1.narvii.com/6162/3b1affdff1083383b3a97256a1cf22caa1e8da8a_00.jpg'),
     (1, 'Second Event!', '2022-04-25', 'Just in case...', 'https://animedrawn.com/wp-content/uploads/2020/05/Chopper-683x1024.png'),
     (2, 'First Post!', '2022-04-25', 'An explanation', 'https://static.wikia.nocookie.net/onepiece/images/e/e6/Tony_Tony_Chopper_Anime_Pre_Timeskip_Infobox.png/revision/latest?cb=20160207143020'),
-    (2, 'And another post', '2022-04-25', 'I hate images', null);
+    (2, 'And another post', '2022-04-25', 'I hate images', null),
+    (2, 'And another post2', '2022-04-25', 'type some stuff out', null);
     
 set sql_safe_updates = 1;
 end //
